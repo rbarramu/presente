01394819bc76dc200c8ae1000f38ba260c527ca7
Rocio Barramuño <rocio@Rocios-MacBook-Pro.local>
2020-05-24 23:43:53 -0400

    changes in draw.c

--- src/draw.c
+++ src/draw.c
@@ -45,6 +45,10 @@ void draw_state(const level *lvl, const state *sta){
         // Draw a circle with the radius of the entity, color depends on the enemy type
         if(sta->enemies[i].kind == MINION){
             DrawCircleV(vec,ent.rad,YELLOW);
+        }else if(sta->enemies[i].kind == SHOOTER){
+            DrawCircleV(vec,ent.rad,BLACK);
+        }else if (sta->enemies[i].kind == KILLER){
+            DrawCircleV(vec,ent.rad,ORANGE);
         }else{
             DrawCircleV(vec,ent.rad,RED);
         }
